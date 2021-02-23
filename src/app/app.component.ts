a3886e42177177b6037ab1a0ef0b9510fc8780b0
MarcoOctav64 <c14180017@john.petra.ac.id>
2021-02-23 19:50:01 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 0;
+
+  item = [
+    { nama: "jeruk", harga: 15000, satuan: "kg" },
+    { nama: "Apel", harga: 5000, satuan: "kg" }
+  ];
+
+  tampil: boolean = true;
 }
